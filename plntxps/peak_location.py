cd9eae16b1507b4b3aa38e5db39b6865ab92c86f
Christian Geci <christian.geci@maine.edu>
2026-02-10 13:43:47 -0500

    mostly adding type hints and docstrings at this point

--- plntxps/peak_location.py
+++ plntxps/peak_location.py
@@ -2,5 +2,9 @@ from dataclasses import dataclass
 
 @dataclass
 class PeakLocation:
+    """
+    Contains the location of an XPS at a specific point in time
+    and the uncertainty in the peak position
+    """
     value: float
     uncertainty: float
--- plntxps/spectrum.py
+++ plntxps/spectrum.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,21 +7,39 @@ from .readutils import get_data, get_time, get_region, get_comment
 
 @dataclass
 class Spectrum:
+    "Contains an XPS spectrum"
     counts: np.ndarray
+    "XPS signal"
     eV: np.ndarray
+    "XPS binding energy (in eV)"
     name: str
+    "Name of the XPS region (e.g. Ti 2p, O 1s, etc.)"
     comment: str
     time: float
+    "Timestamp of spectrum acquisition"
     child_operations: list
+    "Operations (e.g. peak area, multi-peak fit) which reference this spectrum"
     charge_correction: float = None
+    "Shift applied to binding energy to account for charging effects"
     def plot(self, ax = plt, **kwargs):
         ax.plot(self.eV, self.counts, **kwargs)
 
     @property
-    def eV_corrected(self):
+    def eV_corrected(self) -> np.ndarray:
+        "Charge corrected binding energy (in eV)"
         return self.eV + self.charge_correction
     
-    def slice(self, slice_min, slice_max):
+    def slice(self, slice_min: float, slice_max: float) -> Spectrum:
+        """
+        Slice the spectrum object and return a new spectrum based on the slice
+        
+        :param slice_min: Lower binding energy range
+        :type slice_min: float
+        :param slice_max: Upper binding energy range
+        :type slice_max: float
+        :return: Sliced spectrum
+        :rtype: Spectrum
+        """
         packed_data = self.eV, self.counts
         transposed_data = list(zip(*packed_data))
         filtered_data = [
@@ -32,7 +51,15 @@ class Spectrum:
         return Spectrum(counts=counts, eV=eV, name=None, comment=None,
                         time=None, child_operations=None, charge_correction=None)
 
-def read_spectrum(entry):
+def read_spectrum(entry: str) -> Spectrum:
+    """
+    Read spectrum from data file section
+    
+    :param entry: Section of data file that contains spectrum data and metadata
+    :type entry: str
+    :return: Spectrum object
+    :rtype: Spectrum
+    """
     eV, counts = get_data(entry)
     time = get_time(entry)
     name = get_region(entry)
