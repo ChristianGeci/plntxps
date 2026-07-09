e4f64fc68f82c28ee5307eb60ba0e078980b9e06
Christian Geci <christian.geci@maine.edu>
2026-07-09 15:00:45 -0400

    successfuly test of plot_peaks

--- plntxps/xpsdb.py
+++ plntxps/xpsdb.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from importlib.resources import files
@@ -21,8 +22,40 @@ def get_positions_and_names(element, df):
         .apply(lambda x: f"{element} {x}")).tolist()
     return positions, levels
 
-def plot_peaks(element, vline_min, vline_max, **kwargs):
+def plot_core_peaks(element, vline_min, vline_max,
+        minimum_distance = 100,
+        **kwargs):
     positions, names = get_positions_and_names(element, photoemission_df)
-    plt.vlines(positions, vline_min, vline_max, **kwargs)
-    for position, name in zip(positions, names):
-        plt.annotate(name, (position, vline_max), rotation = 45)
+    line = plt.vlines(positions, vline_min, vline_max, **kwargs)
+    adjusted_positions = simulate_node_repulsion(
+        positions, minimum_distance, minimum_distance/10)
+    for position, name in zip(adjusted_positions, names):
+        plt.annotate(name, (position, vline_max), rotation = 45, color = line.get_color())
+
+def simulate_node_repulsion(initial_positions, minimum_distance, granularity):
+    def velocity_from_one_point(point_position, neighbor_position):
+        distance_vector = neighbor_position - point_position
+        if np.abs(distance_vector) >= minimum_distance:
+            return 0
+        if distance_vector == 0 :
+            return 0
+        direction = -np.sign(distance_vector)
+        magnitude = np.min([minimum_distance, np.abs(1 / distance_vector * granularity)])
+        return direction * magnitude
+    def get_velocities(positions):
+        velocities = []
+        for position in positions:
+            velocities.append(0)
+            for neighbor in positions:
+                velocities[-1] += velocity_from_one_point(position, neighbor)
+        velocities = np.array(velocities)
+        return velocities
+
+    position_snapshots = [initial_positions]
+    while True:
+        velocities = get_velocities(position_snapshots[-1])
+        if (velocities == np.zeros(len(velocities))).all():
+            break
+        position_snapshots.append(position_snapshots[-1] + velocities)
+
+    return position_snapshots[-1]
