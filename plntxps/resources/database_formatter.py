2f84e840ef6e79e030fc23207c696d8a990b8dd1
Christian Geci <christian.geci@maine.edu>
2026-07-10 15:47:50 -0400

    format auger electron database

--- plntxps/resources/database_formatter.py
+++ plntxps/resources/database_formatter.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import re
 
-photoemission_df = pd.read_csv("HandbookXPS.csv", sep = ';')
-
-print(photoemission_df)
-formatted_photoemission_df = pd.DataFrame()
-
 def str_to_float(string):
     return float(re.sub(r'[^0-9.]', "", string))
+
+# Direct Photoemission
+
+photoemission_df = pd.read_csv("HandbookXPS.csv", sep = ';')
+formatted_photoemission_df = pd.DataFrame()
 
 binding_energies = []
 for index, row in photoemission_df.iterrows():
@@ -17,7 +17,21 @@ formatted_photoemission_df["Element"] = photoemission_df["AtomicLevel.symbol"]
 formatted_photoemission_df["Level"] = photoemission_df["AtomicLevel.level"]
 formatted_photoemission_df["Binding Energy"] = binding_energies
 
-print(formatted_photoemission_df)
-
 formatted_photoemission_df.to_csv("HandbookXPS_formatted.csv",
+    sep = '\t', index = False)
+
+# Auger Emission
+
+auger_df = pd.read_csv("HandbookAES.csv", sep = ';')
+formatted_auger_df = pd.DataFrame()
+
+kinetic_energies = []
+for index, row in auger_df.iterrows():
+    kinetic_energies.append(str_to_float(row["KineticEnergy"]))
+
+formatted_auger_df["Element"] = auger_df["AtomicLevel.symbol"]
+formatted_auger_df["Level"] = auger_df["AtomicLevel.level"]
+formatted_auger_df["Kinetic Energy"] = kinetic_energies
+
+formatted_auger_df.to_csv("HandbookAES_formatted.csv",
     sep = '\t', index = False)
